2a4a2e9b18dd36bc178e47aa97d6c04d38c494cb
BuildTools <Sanderhgtf@gmail.com>
2023-11-22 23:27:20 +0100

    commit

--- public/script.js
+++ public/script.js
@@ -15,30 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-// navbar disapear on scroll
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get the header element
-    var header = document.querySelector('header');
+// navbar disappear on scroll
+document.addEventListener('DOMContentLoaded', function () {
+  var header = document.querySelector('header');
+  var scrollThreshold = 0.3 * document.documentElement.scrollHeight;
 
-    // Get the height of the header
-    var headerHeight = header.clientHeight;
+  window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
 
-    // Calculate the scroll position for hiding/showing the header
-    var scrollThreshold = 0.3 * document.documentElement.scrollHeight;
-
-    // Add an event listener for scroll events
-    window.addEventListener('scroll', function () {
-      // Get the current scroll position
-      var scrollPosition = window.scrollY;
-
-      // Check if the scroll position is beyond the threshold
-      if (scrollPosition > scrollThreshold) {
-        // If beyond the threshold, add a class to hide the header
-        header.classList.add('hidden');
-      } else {
-        // If before the threshold, remove the class to show the header
-        header.classList.remove('hidden');
-      }
-    });
+    if (scrollPosition > scrollThreshold) {
+      header.classList.add('hidden');
+    } else {
+      header.classList.remove('hidden');
+    }
   });
-
+});
