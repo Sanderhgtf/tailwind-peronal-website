e00c1ab293d9eca111113f62a6604acbb1130e15
BuildTools <Sanderhgtf@gmail.com>
2023-11-23 12:14:18 +0100

    - Now a one page website. - Header now appears/disappears.

--- public/script.js
+++ public/script.js
@@ -15,18 +15,49 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-// navbar disappear on scroll
-document.addEventListener('DOMContentLoaded', function () {
-  var header = document.querySelector('header');
-  var scrollThreshold = 0.3 * document.documentElement.scrollHeight;
+// Smooth href scroll.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-  window.addEventListener('scroll', function () {
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      const scrollOptions = {
+          behavior: 'smooth',
+          block: 'start',
+          inline: 'nearest'
+      };
+
+      // Hard code the duration to be double as quick as the standard
+      const defaultDuration = 1000; // You can adjust this value
+      scrollOptions.duration = defaultDuration;
+
+      targetElement.scrollIntoView(scrollOptions);
+  });
+});
+
+// Header disappearing and appearing
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector("header");
+  var navHeight = header.offsetHeight;
+  var scrollThreshold = window.innerHeight * 0.5; // Adjust the percentage as needed
+  var isHovered = false;
+
+  function updateHeaderVisibility() {
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > scrollThreshold) {
-      header.classList.add('hidden');
+    // Check if scrolled past the threshold or hovered at the top
+    if ((isHovered || scrollPosition < scrollThreshold)) {
+      header.classList.remove("header-hidden", "header-no-shadow");
     } else {
-      header.classList.remove('hidden');
+      header.classList.add("header-hidden", "header-no-shadow");
     }
+  }
+
+  window.addEventListener("scroll", updateHeaderVisibility);
+
+  window.addEventListener("mousemove", function (event) {
+    // Check if the mouse is near the top of the view window
+    isHovered = event.clientY < 50; // Adjust the value as needed
+    updateHeaderVisibility();
   });
 });
